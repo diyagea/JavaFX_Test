9624e0883721ef6cf3bb8a11be0749d15745dddd
diyagea <diyagea@qq.com>
2018-07-10 10:04:12 +0800

    Swing for JavaFX

---
+++ src/SwingFX/SwingFx.java
@@ -0,0 +1,36 @@
+package SwingFX;
+
+import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javax.swing.JButton;
+import javax.swing.SwingUtilities;
+
+public class SwingFx extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		final SwingNode swingNode = new SwingNode();
+
+		createSwingContent(swingNode);
+
+		StackPane pane = new StackPane();
+		pane.getChildren().add(swingNode);
+
+		stage.setTitle("Swing in JavaFX");
+		stage.setScene(new Scene(pane, 250, 150));
+		stage.show();
+	}
+
+	private void createSwingContent(final SwingNode swingNode) {
+		SwingUtilities.invokeLater(() -> {
+			swingNode.setContent(new JButton("Click me!"));
+		});
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
