555ca0fdadb336371f9ec746038624713e121520
diyagea <diyagea@qq.com>
2018-07-30 11:20:53 +0800

    treetable

---
+++ src/Table/TreeTable/Data.java
@@ -0,0 +1,22 @@
+package Table.TreeTable;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Data {
+
+	private final StringProperty data;
+
+	public Data(String data) {
+		this.data = new SimpleStringProperty(data);
+	}
+
+	public StringProperty dataProperty() {
+		return data;
+	}
+
+	@Override
+	public String toString() {
+		return data.get();
+	}
+}
